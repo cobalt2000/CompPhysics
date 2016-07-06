0a9fb6820687ced330c2f5b69e8ea886abc0e2f8
Derrick Cerwinsky <huntsman7@hotmail.com>
2016-07-06 17:48:08 -0400

    The start of the Eigen solver code.

---
+++ LAPACK_Samples/EigenProblem/eigenVec.h
@@ -0,0 +1,18 @@
+
+
+
+
+extern "C" void dsyev( char* jobz, 
+		   char* uplo, 
+		   int* n, 
+		   double* a, 
+		   int* lda, 
+		   double* w, 
+		   double* work, 
+		   int* lwork, 
+		   int* info );
+
+
+
+
+
