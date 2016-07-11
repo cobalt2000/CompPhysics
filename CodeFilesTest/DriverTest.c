59e73c86a752d0b6fdd064fb80306f27a613abcc
Valerie Plaus <vplaus@valeries-mbp.attlocal.net>
2016-07-11 11:29:40 -0400

    First test at creating a driver.

---
+++ CodeFilesTest/DriverTest.c
@@ -0,0 +1,49 @@
+//
+//  DriverTest.c
+//  
+//
+//  Created by Valerie Plaus on 7/11/16.
+//
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "HiggsLoops.h"
+
+/*****************************************
+ A first try at writing a driver.
+ *****************************************/
+
+int main(int argc, char** argv){
+    
+    int i,j, k;  // Standard indexs
+//    int info;    // This is a flag used to tell if the function worked correctly.
+    
+    double Yteff=ytsm*2; // Effective top coupling in the new model
+    double Ybeff=ybsm*2; // Effective bottom coupling in the new model
+    double higgsmass=130; // Test mass top coupling in the new model
+    double vev = 246.0;
+    double mt = 173; //Mass of the top particle, Particle Data Group 2015 Summary Tables
+    double mb = 4.66; //Mass of the bottom particle, Particle Data Group 2015 Summary Tables
+    double ytsm = sqrt(2.0)*mt/vev;  //Standard Model value for the top Yukawa coupling
+    double ybsm = sqrt(2.0)*mb/vev; //Standard Model value for the bottom Yukawa coupling
+    double xsgaga;
+    double xsglgl;
+    
+    printf("The SM and effective top couplings are: %lg and %lg", ytsm, Yteff);
+    printf("The SM and effective bottom couplings are: %lg and %lg", ybsm, Ybeff);
+    printf("The sample Higgs mass is: %lg", higgsmass);
+    
+    
+    // Call solver
+    xsgaga=h2gaga( higgsmass, Yteff, Ybeff );
+    xsglgl=h2glgl( higgsmass, Yteff, Ybeff );
+    
+    // Print solution
+    printf("The ratio of the Higgs production from gamma-gamma with respect to the SM is %lg",xsgaga)
+    printf("The ratio of the Higgs production from gluon-gluon with respect to the SM is %lg",xsglgl)
+    
+    
+    return 0;
+    
+}
