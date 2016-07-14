8a562547f05b2c8f1248e616fa85ce22cc8f514e
Valerie Plaus <vplaus@valeries-mbp.attlocal.net>
2016-07-14 19:28:21 -0400

    I'm not sure how to break down this giant mess of a function.

---
+++ CodeFilesTest/Branching_Frac.h
@@ -0,0 +1,17 @@
+//
+//  Branching_Frac.h
+//  
+//
+//  Created by Valerie Plaus on 7/14/16.
+//
+//
+
+#ifndef Branching_Frac_h
+#define Branching_Frac_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <complex.h>
+
+#endif /* Branching_Frac_h */
